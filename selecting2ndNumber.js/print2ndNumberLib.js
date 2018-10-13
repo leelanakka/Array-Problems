ab8f99d2ddaa0d252aa4006005f2a96a56a4d434
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-10-13 22:29:10 +0530

    i have changed the file names
    i moved the same into another folder

--- selecting2ndNumber.js/print2ndNumberLib.js
+++
@@ -1,9 +0,0 @@
-let selected2ndNumberArray = function(numbersArray){
-  let resultArray = [];
-  for(let index = 1; index<numbersArray.length; index+=2){
-    resultArray.push(numbersArray[index]);
-  }
-  return resultArray;
-}
-
-exports.resultArray = selected2ndNumberArray;
