80980dd2ab8ac4ae14a30badd2dd0e51de723717
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-10-14 14:55:59 +0530

    in this file I have tested for 1 and two numbers and threshold limit as 0

--- countNumbersAboveThreshold/sourceFile.js
+++ countNumbersAboveThreshold/sourceFile.js
@@ -1,6 +1,6 @@
 let countNumbersAboveThreshold = function(numbersArray,limit){
   let result = 0;
-  return 1
+  return numbersArray.length;
 }
 exports.result = countNumbersAboveThreshold;
 
--- countNumbersAboveThreshold/threshold_test.js
+++ countNumbersAboveThreshold/threshold_test.js
@@ -3,3 +3,5 @@ const result = require('./sourceFile.js').result;
 
 //if I give 1 in an array and limit as 0 it should print 1
 assert.equal(result([1],0),1);
+// if I give 1,2 in an aray and limit as 0 it should print 2
+assert.equal(result([1,2],0),2);
