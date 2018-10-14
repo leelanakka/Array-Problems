ab334c7d037f2f17e203056540930a039a708b0d
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-10-14 18:39:32 +0530

    this is the new version file
    in this file I have managed the letters.
    if the user gives in the string it wont show any error
    it just considers as not a odd number

--- oddNumbers/oddNumbersLib.js
+++ oddNumbers/oddNumbersLib.js
@@ -1,7 +1,7 @@
 let isOdd=function(oddCandidate){
   let result=[];
   for(let index = 0; index<oddCandidate.length; index++){
-    if(oddCandidate[index]%2 != 0){
+    if(oddCandidate[index]%2 == 1){
       result.push(oddCandidate[index]);
     }
   }
--- oddNumbers/testLib.js
+++ oddNumbers/testLib.js
@@ -3,3 +3,6 @@ const result = require('./oddNumbersLib.js').result;
 
 assert.deepEqual(result([1,2,3]),[1,3])
 assert.deepEqual(result([11,22,23,34]),[11,23])
+assert.deepEqual(result([11,22,23,34,"a"]),[11,23])
+
+assert.deepEqual(result(["a","b","c"]),[])
