4aae5cdcea48b805d3fb7d9715116dc0587c4fe9
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-10-14 15:12:22 +0530

    in this I tested for both positive and negative numbers

--- countNumbersAboveThreshold/threshold_test.js
+++ countNumbersAboveThreshold/threshold_test.js
@@ -7,3 +7,5 @@ assert.equal(result([1],0),1);
 assert.equal(result([1,2],0),2);
 //if I give 2,3 in an array and limit as 3 it should print 0
 assert.equal(result([2,3],4),0);
+//if I give -1 in an array and limit as -2 it should print 1
+assert.equal(result([-1],-2),1)
